c8ef5026cea2a8faf50ae05bfc1278730f3d07de
Mamedov Murad <mamedov_99b@mail.ru>
2022-11-14 12:24:15 +0300

    [FIX]

--- interface/src/forms.py
+++ interface/src/forms.py
@@ -1,9 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import ValidationError, Length, DataRequired
+from wtforms import IntegerField, StringField, PasswordField, SubmitField
+from wtforms.validators import Length, DataRequired
 
 
 class SignInForm(FlaskForm):
     username = StringField(label='Username:', validators=[Length(min=2, max=255), DataRequired()])
     password = PasswordField(label='Password:', validators=[Length(min=4, max=255), DataRequired()])
     submit = SubmitField(label='Sign in')
+
+    def is_valid(self) -> bool:
+        import src.settings as settings
+        if self.username == settings.ADMIN_USERNAME and self.password == settings.ADMIN_PASSWORD:
+            return True
+        return False
+
+
+class SignIn2FAForm(FlaskForm):
+    code = IntegerField(label='Code:', validators=[Length(min=6, max=6), DataRequired()])
+    submit = SubmitField(label='Sign in')
+
+    def is_valid(self) -> bool:
+        pass
+
--- interface/src/views/auth.py
+++ interface/src/views/auth.py
@@ -2,30 +2,46 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import UserMixin, login_user, login_required, logout_user, current_user
 
 import src.settings as settings
-from ..forms import SignInForm
 from ..services.auth import Admin
+
+
+class AdminAuth:
+    pass
 
 
 app = Blueprint('auth', __name__)
 
 
-def _valid_admin(username: str, password: str) -> UserMixin:
-    if username == settings.ADMIN_USERNAME and password == settings.ADMIN_PASSWORD:
-        return Admin(username=username)
-
-
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    from ..forms import SignInForm
     form = SignInForm()
     if form.validate_on_submit():
-        admin = _valid_admin(form.username.data, form.password.data)
-        if admin:
+        if form.is_valid():
+            admin = Admin(username=form.username.data)
+            if settings.ADMIN_2AF:
+                return redirect(url_for('auth.login_2fa'))
             login_user(admin)
-            return redirect(url_for('main.index'))
+            return redirect(url_for('auth.login_2fa'))
         else:
             flash('Username and password are not match! Please try again', category='danger')
 
     return render_template(
-        'login.html',
+        'auth/login.html',
         form=form
     )
+
+
+@app.route("/login/2fa/<username>", methods=['POST', 'GET'])
+def login_2fa(username: str):
+    from ..forms import SignIn2FAForm
+    form = SignIn2FAForm()
+    return render_template(
+        'auth/login_2fa.html',
+        form=form
+    )
+
+
+@app.route('/logout')
+def logout():
+    pass
