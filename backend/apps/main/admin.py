62f96b149f488cb5ed4552b2e97aa81019f24bcf
Mamedov Murad <mamedov_99b@mail.ru>
2022-11-17 16:16:03 +0300

    [ADD] Account view

--- backend/apps/main/admin.py
+++ backend/apps/main/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
+from .models import Account
 from .models import Google2FA
 from .models import Telegram
-from .models import Account
 
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'network', 'active', 'user')
+    list_filter = ('network', 'active', 'user')
+
+
+admin.site.register(Account, AccountAdmin)
