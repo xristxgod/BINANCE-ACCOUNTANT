dc36cead5abd13e4ea16ba63e9d4cbaeae4a6125
Mamedov Murad <mamedov_99b@mail.ru>
2022-11-22 12:03:00 +0300

    [ADD] wallet daemon

--- daemons/wallet-daemon/gateway/gate/tron.py
+++ daemons/wallet-daemon/gateway/gate/tron.py
@@ -1,19 +1,27 @@
+import decimal
 import logging
+from dataclasses import dataclass
 from typing import NoReturn, List
 
 import src.settings as settings
 import gateway.gate.base as base
 
 
+@dataclass()
+class BodyCreateTransaction:
+    transactionHash: str
+    fee: str
+
+
 class BlockManager(base.BaseBlockManager):
     file_path = settings.BLOCKS_DIR + 'tron_block.txt'
 
-    def __init__(self, gate, *args, **kwargs):
+    def __init__(self, gate, **kwargs):
         from tronpy.async_tron import AsyncTron
 
         self.gate: AsyncTron = gate
 
-        super(BlockManager, self).__init__(*args, **kwargs)
+        super(BlockManager, self).__init__(**kwargs)
 
     async def get_block_by_id(self, block: int) -> List[base.DefaultBlock]:
         block = await self.gate.get_block(block)
@@ -34,7 +42,19 @@ class BlockManager(base.BaseBlockManager):
 
 
 class TransactionManager(base.BaseTransactionManager):
-    pass
+
+    def __init__(self, gate, **kwargs):
+        from tronpy.async_tron import AsyncTron
+
+        self.gate: AsyncTron = gate
+
+        super(TransactionManager, self).__init__(**kwargs)
+
+    async def create_transaction(self, from_: str, to: str, amount: decimal.Decimal) -> BodyCreateTransaction:
+        pass
+
+    async def send_transaction(self, transaction_hash: str, private_key: str) -> base.DefaultTransaction:
+        pass
 
 
 class Node(base.BaseNode):
